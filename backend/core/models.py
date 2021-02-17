b26f26641dfa54cc12bd819b862be15676e244a5
Sean McNeil <sean.mcneil@beatport.com>
2021-02-17 14:04:21 -0700

    order by upvotes and created

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Chirp(models.Model):
     id = models.AutoField(primary_key=True)
     text = models.CharField(max_length=140)
     upvotes = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
 
+    # TODO: finish with recency score
+    # @property
+    # def recency_score(self):
+    #     hours_ago = self.created - timezone.now()
+    #     print(hours_ago)
+    #     return self.upvotes * float(1/hours_ago.hours)
     class Meta:
         managed = True
-        ordering = ('-id',)
+        ordering = ('-upvotes','-created')
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from core.serializers import ChirpSerializer
 from core.models import Chirp
+from rest_framework.response import Response
 
 class ChirpViewSet(viewsets.ModelViewSet):
     """
@@ -8,3 +9,11 @@ class ChirpViewSet(viewsets.ModelViewSet):
     """
     queryset = Chirp.objects.all()
     serializer_class = ChirpSerializer
+
+    # TODO: finish if wanting to sort by votes
+    # def list(self, request, *args, **kwargs):
+    #     queryset = self.filter_queryset(self.get_queryset())
+    #     ## Ranking upvotes * 1/(number of hours ago)
+    #     queryset = sorted(queryset, key=lambda t: t.recency_score)
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
